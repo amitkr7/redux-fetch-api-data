d744c2c525108e0d521329a142459645fa453185
Amit <amit840901@gmail.com>
2020-11-07 02:53:53 +0530

    dispatch data from reducer and render list

---
+++ src/reducers/postsReducer.js
@@ -0,0 +1,9 @@
+export default function postsReducers(state = [], action) {
+  switch (action.type) {
+    case 'FETCH_POSTS':
+      return action.payload;
+
+    default:
+      return state;
+  }
+}
