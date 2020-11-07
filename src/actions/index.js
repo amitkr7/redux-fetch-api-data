4e9991ed76c6c57883535a22c4a63e41abfbb2a6
Amit <amit840901@gmail.com>
2020-11-08 05:24:48 +0530

    create usersReducer and UserHeader Component

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,3 +9,12 @@ export const fetchPosts = () => {
     });
   };
 };
+
+//2015 ES Syntax
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({
+    type: 'FETCH_USER',
+    payload: response.data,
+  });
+};
