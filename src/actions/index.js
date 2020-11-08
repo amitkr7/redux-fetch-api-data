9f627549e9475dfbe4e5759ff59c018e4878f69c
Amit <amit840901@gmail.com>
2020-11-08 22:48:07 +0530

    Add memoize to reduce network request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash';
 
 export const fetchPosts = () => {
   return async function (dispatch) {
@@ -11,10 +12,11 @@ export const fetchPosts = () => {
 };
 
 //2015 ES Syntax
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = (id) => (dispatch) => _fetchuser(id, dispatch);
+const _fetchuser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({
     type: 'FETCH_USER',
     payload: response.data,
   });
-};
+});
